ec30ceee4f0674e4a65fa7d22371293599c2b07f
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2025-03-01 13:36:13 +0700

    update code

---
+++ android/app/src/main/kotlin/roni/pnp/berita/flutter/flutter_berita/MainActivity.kt
@@ -0,0 +1,5 @@
+package roni.pnp.berita.flutter.flutter_berita
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_berita/pages/home_page.dart';
+import 'package:flutter_berita/pages/login_page.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (_) => HomePage(),
+        ),
+      );
+    });
+
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("images/logo.png"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
