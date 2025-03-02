95aea8c87a204550b2d77475bf1ffe213d6b9548
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2025-03-02 17:20:16 +0700

    update code

--- lib/pages/berita_add_page.dart
+++ lib/pages/berita_add_page.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:logger/logger.dart';
+
 
 class BeritaAddPage extends StatefulWidget {
   const BeritaAddPage({super.key});
