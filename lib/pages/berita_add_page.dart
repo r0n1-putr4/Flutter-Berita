e956f9f5727a77eff3cb67fd43bdbf6e3c82267e
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2025-03-02 17:15:29 +0700

    update code

---
+++ lib/pages/berita_add_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BeritaAddPage extends StatefulWidget {
+  const BeritaAddPage({super.key});
+
+  @override
+  State<BeritaAddPage> createState() => _BeritaAddPageState();
+}
+
+class _BeritaAddPageState extends State<BeritaAddPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
